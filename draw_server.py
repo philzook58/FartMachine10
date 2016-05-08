42f7c99137856a1287e21fa008b93744ec12327d
Ben <ishmandoo@gmail.com>
2016-05-08 10:22:37 -0400

    added general draw function

--- draw_server.py
+++ draw_server.py
@@ -1,7 +1,21 @@
 from draw import DrawBot
 import sys
+#from flask import Flask
+#from flask import render_template
+import threading
+import time
 
 bot = DrawBot("/dev/ttyUSB0")
 
+
+
 filename = sys.argv[1]
-bot.draw(filename)
+gcode = '''
+G90
+G21
+G1 F3000
+G1  X0.5702 Y0.5702
+'''
+print gcode.split()
+
+bot.draw(gcode.split())
