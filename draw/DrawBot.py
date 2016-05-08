a9c9e8486a97504c616d02eb1292564d7d2b1f68
Ben <ishmandoo@gmail.com>
2016-05-07 22:01:16 -0400

    made drawbot into module

---
+++ draw/DrawBot.py
@@ -0,0 +1,47 @@
+import numpy as np
+import time
+from parse import compile
+import sys
+import serial
+
+class DrawBot:
+    def __init__(self, port, baud = 38400):
+        self.port = port
+        self.ser = serial.Serial(port, baud)  # open serial port
+    def draw(file_name):
+        x = np.zeros(2)
+        self.raisepen()
+        file = open(file_name)
+
+        print self.ser.readline()
+
+        for line in file:
+            if line[0:2] == "G1":
+                command = p.parse(line)
+                if command != None:
+                    command = command.named
+                    x = np.array([float(command['x']),float(command['y'])])
+                    self.move_to(x)
+
+            elif line[0:3] == "M05":
+                self.raisepen()
+            elif line[0:3] == "M03":
+                self.lowerpen()
+    def __del__(self):
+        self.ser.close()
+    def raisepen(self):
+        self.ser.write('50c')
+    def lowerpen(self):
+        self.ser.write('40c')
+    def move(self,steps):
+        self.ser.write(str(int(steps[0])) + b'a')
+        self.ser.write(str(int(steps[1])) + b'b')
+        self.ser.write(b'x')
+    def convertToMotor(x):
+        return np.array([x[0]+x[1],x[0]-x[1]])/np.sqrt(2)
+    def move_to(self,x):
+        x = convertToMotor(x)
+        self.ser.write(str(int(x[0])) + b'a')
+        self.ser.write(str(int(x[1])) + b'b')
+        self.ser.write(b'x')
+        print ser.readline()
